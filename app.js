016e32f4cb30ee4cc3923ab4d6e19e5ba6d414ba
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-09-09 16:39:05 -0300

    Aprendendo a usufruir de uma Api com a Api 'Dog API'

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// import imagens from buscarImagem with {type:'json'}
+
+const btnBuscar = document.getElementById('botao')
+const main = document.querySelector('main')
+btnBuscar.addEventListener('click', criarImagens)
+
+
+async function criarImagens(){
+    const raca = document.getElementById('search').value
+    const imagens = await buscarImagem(raca)
+    main.textContent = ""    
+    imagens.forEach((url) => {
+        const img = document.createElement('img')
+        img.src = url
+
+        main.appendChild(img)
+    
+    })
+}
+
+async function buscarImagem(raca){
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+    const response = await fetch(url)
+    const imagens = await response.json()
+    return imagens.message
+}
